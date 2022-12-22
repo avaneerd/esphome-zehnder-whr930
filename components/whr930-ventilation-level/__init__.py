74c7a29a80928e44053724d95ff3e1221300ad68
Alexander van Eerd <a.van.eerd@gmail.com>
2022-12-22 20:53:02 +0100

    Fix component registration?

--- components/whr930-ventilation-level/__init__.py
+++ components/whr930-ventilation-level/__init__.py
@@ -8,14 +8,14 @@ CONF_WHR930_VENTILATION_LEVEL_ID = "whr930_ventilation_level_id"
 whr930_ns = cg.esphome_ns.namespace("whr930")
 
 Whr930VentilationLevelComponent = whr930_ns.class_(
-    "Whr930VentilationLevelComponent", cg.Component
+    "Whr930VentilationLevelComponent", cg.PollingComponent
 )
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(Whr930VentilationLevelComponent),
     }
-)
+).extend(cv.polling_component_schema('60s'))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
