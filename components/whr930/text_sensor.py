572ceeb0041ba84b8f94646ee239c0fce4b86b02
Alexander van Eerd <a.van.eerd@gmail.com>
2022-12-23 00:35:54 +0100

    Added supply fan active sensor

--- components/whr930/text_sensor.py
+++ components/whr930/text_sensor.py
@@ -1,29 +1,20 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor
+from esphome.components import text_sensor
 from esphome.const import (
     CONF_ID,
-    DEVICE_CLASS_EMPTY,
-    STATE_CLASS_MEASUREMENT,
-    UNIT_EMPTY,
 )
 
 from . import CONF_WHR930_ID, Whr930
-
-ICON_FAN = "mdi:fan"
 
 AUTO_LOAD = ["whr930"]
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_WHR930_ID): cv.use_id(Whr930),
-        cv.Optional("ventilation_level"): sensor.sensor_schema(
-            unit_of_measurement=UNIT_EMPTY,
-            icon=ICON_FAN,
-            accuracy_decimals=0,
-            device_class=DEVICE_CLASS_EMPTY,
-            state_class=STATE_CLASS_MEASUREMENT,
-        )
+        cv.Optional("supply_fan_active"): text_sensor.text_sensor_schema(
+            icon="mdi:fan-chevron-down"
+        ),
     }
 )
 
@@ -35,7 +26,7 @@ async def to_code(config):
         if not isinstance(conf, dict):
             continue
         id = conf[CONF_ID]
-        if id and id.type == sensor.Sensor:
-            sens = await sensor.new_sensor(conf)
+        if id and id.type == text_sensor.TextSensor:
+            sens = await text_sensor.new_text_sensor(conf)
             cg.add(getattr(hub, f"set_{key}")(sens))
             sensors.append(f"F({key})")
