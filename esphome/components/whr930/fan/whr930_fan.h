00ee7eeff430314070c2f5c1a0c44299531b4307
Alexander van Eerd <a.van.eerd@gmail.com>
2022-12-25 02:59:37 +0100

    Some improvements

--- esphome/components/whr930/fan/whr930_fan.h
+++ esphome/components/whr930/fan/whr930_fan.h
@@ -16,18 +16,20 @@ class Whr930Fan : public PollingComponent, public fan::Fan {
     whr930_(whr930),
     fan_type_(&fan_type),
     PollingComponent(60000) {
+      data_index = fan_type == FanType::EXHAUST ? 1 : 4;
    }
 
   const uint8_t min_speed_level = 45;
   const uint8_t max_speed_level = 100;
   uint8_t response_bytes[13];
   uint8_t data_bytes[10] = { 15, 45, 75, 15, 45, 75, 100, 100, 1, 1 };
+  int data_index = 1;
 
   void update() override {
     const uint8_t command_byte = 0xCD;
     const uint8_t expected_response_byte = 0xCE;
     if (this->whr930_->execute_request(command_byte, 0, 0, expected_response_byte, response_bytes)) {
-      this->speed = response_bytes[*this->fan_type_ == FanType::EXHAUST ? 1 : 4];
+      this->speed = response_bytes[data_index];
       this->state = *this->fan_type_ == FanType::EXHAUST || response_bytes[9] == 1;
       this->publish_state();
     }
@@ -60,7 +62,7 @@ class Whr930Fan : public PollingComponent, public fan::Fan {
 
     this->speed = new_speed;
 
-    data_bytes[*this->fan_type_ == FanType::EXHAUST ? 1 : 4] = this->speed;
+    data_bytes[data_index] = this->speed;
     this->whr930_->execute_command(command_byte, data_bytes, 10);
 
     this->publish_state();
