955a972c3ee39d63e5c04bf789703f3d85a7fdc7
Alexander van Eerd <a.van.eerd@gmail.com>
2022-12-29 23:14:00 +0100

    Add WHR930 temperature sensors

--- esphome/components/whr930/number/__init__.py
+++ esphome/components/whr930/number/__init__.py
@@ -18,10 +18,10 @@ AUTO_LOAD = ["whr930"]
 
 CONFIG_SCHEMA = cv.All(
     {
-        cv.Optional("comfort_temperature"): number.NUMBER_SCHEMA.extend(cv.COMPONENT_SCHEMA).extend(
+        cv.GenerateID(CONF_WHR930_ID): cv.use_id(Whr930),
+        cv.GenerateID(): cv.declare_id(Whr930ComfortTemperature),
+        cv.Required("comfort_temperature"): number.NUMBER_SCHEMA.extend(cv.COMPONENT_SCHEMA).extend(
         {
-            cv.GenerateID(): cv.declare_id(Whr930ComfortTemperature),
-            cv.GenerateID(CONF_WHR930_ID): cv.use_id(Whr930),
             cv.Optional(CONF_UNIT_OF_MEASUREMENT, default=UNIT_CELSIUS): cv.string_strict,
             cv.Optional(CONF_ACCURACY_DECIMALS, default=0): cv.int_,
         })
@@ -29,9 +29,9 @@ CONFIG_SCHEMA = cv.All(
 )
 
 async def to_code(config):
+    parent = await cg.get_variable(config[CONF_WHR930_ID])
+    id = config[CONF_ID]
     conf = config["comfort_temperature"]
-    id = conf[CONF_ID]
-    parent = await cg.get_variable(conf[CONF_WHR930_ID])
     var = cg.new_Pvariable(id, parent)
     await cg.register_component(var, conf)
     await number.register_number(var, conf, min_value=15, max_value=25, step=1)
---
+++ esphome/components/whr930/sensor/__init__.py
@@ -0,0 +1,70 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor
+from esphome.const import (
+    CONF_ID,
+    UNIT_CELSIUS,
+    DEVICE_CLASS_TEMPERATURE,
+    STATE_CLASS_MEASUREMENT
+)
+
+from .. import CONF_WHR930_ID, whr930_ns, Whr930
+
+Whr930Temperatures = whr930_ns.class_(
+    "Whr930Temperatures", cg.PollingComponent
+)
+
+AUTO_LOAD = ["whr930"]
+
+CONFIG_SCHEMA = cv.All(
+    {
+        cv.GenerateID(CONF_WHR930_ID): cv.use_id(Whr930),
+        cv.GenerateID(): cv.declare_id(Whr930Temperatures),
+        cv.Optional("t1_temperature"): sensor.sensor_schema(
+            unit_of_measurement=UNIT_CELSIUS,
+            device_class=DEVICE_CLASS_TEMPERATURE,
+            state_class=STATE_CLASS_MEASUREMENT,
+            accuracy_decimals=1).extend(cv.COMPONENT_SCHEMA),
+        cv.Optional("t2_temperature"): sensor.sensor_schema(
+            unit_of_measurement=UNIT_CELSIUS,
+            device_class=DEVICE_CLASS_TEMPERATURE,
+            state_class=STATE_CLASS_MEASUREMENT,
+            accuracy_decimals=1).extend(cv.COMPONENT_SCHEMA),
+        cv.Optional("t3_temperature"): sensor.sensor_schema(
+            unit_of_measurement=UNIT_CELSIUS,
+            device_class=DEVICE_CLASS_TEMPERATURE,
+            state_class=STATE_CLASS_MEASUREMENT,
+            accuracy_decimals=1).extend(cv.COMPONENT_SCHEMA),
+        cv.Optional("t4_temperature"): sensor.sensor_schema(
+            unit_of_measurement=UNIT_CELSIUS,
+            device_class=DEVICE_CLASS_TEMPERATURE,
+            state_class=STATE_CLASS_MEASUREMENT,
+            accuracy_decimals=1).extend(cv.COMPONENT_SCHEMA),
+    }
+)
+
+async def to_code(config):
+    id = config[CONF_ID]
+    parent = await cg.get_variable(config[CONF_WHR930_ID])
+    var = cg.new_Pvariable(id, parent)
+    await cg.register_component(var, config)
+
+    if "t1_temperature" in config:
+        conf = config["t1_temperature"]
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_t1_temperature_sensor(sens))
+
+    if "t2_temperature" in config:
+        conf = config["t2_temperature"]
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_t1_temperature_sensor(sens))
+
+    if "t3_temperature" in config:
+        conf = config["t3_temperature"]
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_t1_temperature_sensor(sens))
+
+    if "t4_temperature" in config:
+        conf = config["t4_temperature"]
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_t1_temperature_sensor(sens))
